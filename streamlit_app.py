38750a8377be7ca0efca625257414d04a07c42e4
Maximilium77 <59537776+Maximilium77@users.noreply.github.com>
2023-08-03 14:25:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,86 +1,39 @@
+import streamlit as st
 import pandas as pd
-from flask import Flask, request, render_template, send_file
-from io import BytesIO
-from pptx import Presentation
-from pptx.util import Inches
-import subprocess
-
-module_name = "Flask"
-
-# Esegui il comando di installazione utilizzando pip
-try:
-    subprocess.check_call(["pip", "install", module_name])
-    print(f"Modulo {module_name} installato con successo.")
-except subprocess.CalledProcessError:
-    print(f"Si è verificato un errore durante l'installazione di {module_name}.")
-
-app = Flask(__name__)
-
 import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
-def create_chart(data):
-    # Esempio di creazione di un grafico utilizzando pandas e matplotlib
-    # Puoi personalizzare questa funzione in base alle tue esigenze
-    # data: DataFrame di pandas contenente i dati da plottare
-    # return: Oggetto figura matplotlib
-
-    # Creazione di una nuova figura specificando il backend "Agg"
+# Funzione per generare il grafico
+def create_chart(data, start_date, end_date):
+    filtered_data = data[(data['DATA'] >= start_date) & (data['DATA'] <= end_date)]
     plt.figure(figsize=(8, 6), tight_layout=True, dpi=100)
-
-    # Esempio di creazione di un grafico a barre
-    data.plot(kind='bar')
+    plt.plot(filtered_data['DATA'], filtered_data['VALORE'])
     plt.title('Grafico')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-
-    # Restituisci l'oggetto figura
+    plt.xlabel('Data')
+    plt.ylabel('Valore')
     return plt.gcf()
 
-def create_ppt(chart, start_date, end_date):
-    prs = Presentation()
-    slide_layout = prs.slide_layouts[5]  # Layout del titolo e contenuto
+# Caricamento del file Excel
+uploaded_file = st.file_uploader("Carica un file Excel", type=["xlsx"])
 
-    slide = prs.slides.add_slide(slide_layout)
-    title = slide.shapes.title
-    title.text = f"Grafico dal {start_date} al {end_date}"
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
 
+    # Interfaccia per selezionare il periodo
+    start_date = st.date_input("Seleziona la data di inizio", min_value=df['DATA'].min(), max_value=df['DATA'].max())
+    end_date = st.date_input("Seleziona la data di fine", min_value=df['DATA'].min(), max_value=df['DATA'].max())
+
+    # Crea il grafico
+    chart = create_chart(df, start_date, end_date)
+    st.pyplot(chart)
+
+    # Salva il grafico come immagine e permetti di scaricarlo
     img_buffer = BytesIO()
-    chart.savefig(img_buffer)
+    chart.savefig(img_buffer, format='png')
     img_buffer.seek(0)
-    left = Inches(2)
-    top = Inches(2)
-    pic = slide.shapes.add_picture(img_buffer, left, top)
-
-    return prs
-
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
-
-@app.route('/process_excel', methods=['POST'])
-def process_excel():
-    excel_file = request.files['excelFile']
-    start_date = request.form['startDate']
-    end_date = request.form['endDate']
-
-    # Carica il file Excel e crea un DataFrame con pandas
-    df = pd.read_excel(excel_file)
-
-    # Converti le colonne contenenti dati temporali in formato datetime di pandas
-    df['DATA'] = pd.to_datetime(df['DATA'])
-
-    # Effettua l'elaborazione dei dati e crea il grafico
-    chart = create_chart(df)
-
-    # Crea il PowerPoint e aggiunge il grafico
-    prs = create_ppt(chart, start_date, end_date)
-
-    # Salva il PowerPoint in un buffer di byte
-    ppt_buffer = BytesIO()
-    prs.save(ppt_buffer)
-    ppt_buffer.seek(0)
-
-    return send_file(ppt_buffer, as_attachment=True, attachment_filename='grafici_powerpoint.pptx')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    st.markdown("### Scarica il grafico come immagine:")
+    st.image(img_buffer, use_container_width=True)
+    b64 = base64.b64encode(img_buffer.read()).decode()
+    href = f'<a href="data:image/png;base64,{b64}" download="grafico.png">Clicca qui per scaricare</a>'
+    st.markdown(href, unsafe_allow_html=True)
